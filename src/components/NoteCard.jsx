8070b7c351c78ec9f15b59c0059cbf548d2a0e47
rakeshkunducsgi <rakeshkunducsgi@gmail.com>
2025-08-06 08:04:59 +0000

    Creating Note Components and Rendering Notes

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NoteCard({ note }) {
+    let position = JSON.parse(note.position);
+    const colors = JSON.parse(note.colors);
+    const body = JSON.parse(note.body);
+
+    return (
+        <div className='card' style={{
+            backgroundColor: colors.colorBody,
+        }}>{body}</div>
+    )
+}
+
+export default NoteCard
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {fakeData as notes} from '../assets/fakeData.js'
+import NoteCard from '../components/NoteCard.jsx'
 
 function NotesPage() {
   return (
-    <div>NotesPage</div>
+    <div>
+      {notes.map((note) => {
+        return (
+          <div>
+            <NoteCard key={note.$id} note={note} />
+          </div>
+        )
+      })}
+    {}</div>
   )
 }
 
