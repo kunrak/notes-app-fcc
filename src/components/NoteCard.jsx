eacde06a4624439aac0d0105900d3b15b1ba840b
rakeshkunducsgi <rakeshkunducsgi@gmail.com>
2025-08-22 06:56:20 +0000

    Draggable Note Cards

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,19 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Trash from "../icons/Trash";
 
 function NoteCard({ note }) {
-  let position = JSON.parse(note.position);
+  let mouseStartPos = { x: 0, y: 0 };
+  const cardRef = useRef();
+
+  const [position, setPosition] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
   const body = JSON.parse(note.body);
 
   const textAreaRef = useRef();
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
 
   function autoGrow(textAreaRef) {
     const { current } = textAreaRef;
@@ -14,8 +21,37 @@ function NoteCard({ note }) {
     current.style.height = `${current.scrollHeight}px`;
   }
 
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  }
+
+  const mouseMove = (e) => {
+    let mouseMoveDir = {
+        x: mouseStartPos.x - e.clientX,
+        y: mouseStartPos.y - e.clientY,
+    };
+
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    setPosition({
+      x: cardRef.current.offsetLeft - mouseMoveDir.x,
+      y: cardRef.current.offsetTop - mouseMoveDir.y,
+    })
+  }
+
+  const mouseUp = (e) => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  }
+
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -26,6 +62,7 @@ function NoteCard({ note }) {
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
+        onMouseDown={mouseDown}
       >
         <Trash />
       </div>
