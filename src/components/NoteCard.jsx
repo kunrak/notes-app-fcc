f05f980c8b8149b220e7aa7a32ddcdacd7babcea
rakeshkunducsgi <rakeshkunducsgi@gmail.com>
2025-08-22 05:04:46 +0000

    Final NoteCard Styling(autoGrow)

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import { useRef } from "react";
+import Trash from "../icons/Trash";
 
 function NoteCard({ note }) {
-    let position = JSON.parse(note.position);
-    const colors = JSON.parse(note.colors);
-    const body = JSON.parse(note.body);
+  let position = JSON.parse(note.position);
+  const colors = JSON.parse(note.colors);
+  const body = JSON.parse(note.body);
 
-    return (
-        <div className='card' style={{
-            backgroundColor: colors.colorBody,
-        }}>{body}</div>
-    )
+  const textAreaRef = useRef();
+
+  function autoGrow(textAreaRef) {
+    const { current } = textAreaRef;
+    current.style.height = "auto";
+    current.style.height = `${current.scrollHeight}px`;
+  }
+
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }}
+        ></textarea>
+      </div>
+    </div>
+  );
 }
 
-export default NoteCard
+export default NoteCard;
